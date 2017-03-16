cd1234641f9b7a7775f7f3c64c96bc4cac65cfcc
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2017-03-16 12:21:17 -0700

    Refactor web view

--- app/src/main/java/codepath/com/nytimessearch/activities/ArticleActivity.java
+++ app/src/main/java/codepath/com/nytimessearch/activities/ArticleActivity.java
@@ -1,8 +1,12 @@
 package codepath.com.nytimessearch.activities;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -22,18 +26,36 @@ public class ArticleActivity extends AppCompatActivity {
 
         Article article = (Article) getIntent().getSerializableExtra(ARTICLE_EXTRA);
         WebView webView = (WebView) findViewById(R.id.wvArticle);
-        webView.setWebViewClient(new WebViewClient() {
-//            @Override
-//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//                view.loadUrl(url);
-//                return true;
-//            }
+        // Configure related browser settings
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
 
-        });
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true); // allow pinch to zooom
+        webView.getSettings().setDisplayZoomControls(false); // disable the default zoom controls on the page
 
+        // Configure the client to use when opening URLs
+        webView.setWebViewClient(new MyBrowser());
+        // Load the initial URL
         webView.loadUrl(article.getWebUrl());
+    }
 
+    // Manages the behavior when URLs are loaded
+    private class MyBrowser extends WebViewClient {
+        @SuppressWarnings("deprecation")
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
 
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
     }
 
 }
